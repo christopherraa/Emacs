b985d68d20b63f71693b510cd60a9d84a5053b7d
Christopher Rasch-Olsen Raa <christopher@stix.no>
2013-06-02 23:52:57 +0200

    Lagt til første forsøk på å få multi-web-mode til å fungere med .html.ep-templates.

--- init.el
+++ init.el
@@ -77,3 +77,16 @@
 
 (require 'color-theme-solarized)
 (put 'downcase-region 'disabled nil)
+
+
+; Trying out multi-web-mode
+(require 'multi-web-mode)
+(setq mweb-default-major-mode 'html-mode)
+(setq mweb-tags '((php-mode "<\\?php\\|<\\? \\|<\\?=" "\\?>")
+                  (js-mode "<script[^>]*>" "</script>")
+                  (css-mode "<style[^>]*>" "</style>")
+									(cperl-mode "<%=" "%>")
+									(cperl-mode "^\\(\t\\|\s\\)*%" "$")
+))
+(setq mweb-filename-extensions '("php" "htm" "html" "ctp" "phtml" "php4" "php5" "ep"))
+(multi-web-global-mode 1)
