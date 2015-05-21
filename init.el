b7ef614170c8ce065aa73c576aefbb2e6315b426
Christopher Rasch-Olsen Raa <christopher@stix.no>
2015-05-21 13:35:05 +0200

    Moved dired-mode config to separate file.

--- init.el
+++ init.el
@@ -24,6 +24,7 @@
 (load "setup-company-mode")
 (load "setup-haskell-mode")
 (load "setup-yasnippet")
+(load "setup-dired-mode")
 
 
 ; Make sure matching parens are highlighted, without delay
@@ -87,24 +88,6 @@
 
 ;; don't keep message buffers around
 (setq message-kill-buffer-on-exit t)
-
-; Dired stuff
-(require 'gnus-dired)
-(put 'dired-find-alternate-file 'disabled nil)
-;; make the `gnus-dired-mail-buffers' function also work on
-;; message-mode derived modes, such as mu4e-compose-mode
-(defun gnus-dired-mail-buffers ()
-	"Return a list of active message buffers."
-	(let (buffers)
-		(save-current-buffer
-			(dolist (buffer (buffer-list t))
-				(set-buffer buffer)
-				(when (and (derived-mode-p 'message-mode)
-				           (null message-sent-message-via))
-					(push (buffer-name buffer) buffers))))
-		(nreverse buffers)))
-(setq gnus-dired-mail-mode 'mu4e-user-agent)
-(add-hook 'dired-mode-hook 'turn-on-gnus-dired-mode)
 
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
