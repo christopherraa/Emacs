fbb23f0b3e260de3638164a7a68619dafe4be62e
Christopher Rasch-Olsen Raa <christopher@stix.no>
2015-09-04 13:37:33 +0200

    Added marmalade and gnu repositories for packages.el

---
+++ elisp/setup-packages.el
@@ -0,0 +1,4 @@
+(require 'package)
+(package-initialize)
+(add-to-list 'package-archives '("gnu" . "http://elpa.gnu.org/packages/"))
+(add-to-list 'package-archives '("marmalade" . "http://marmalade-repo.org/packages/"))
--- init.el
+++ init.el
@@ -1,18 +1,11 @@
 ;; Add some path variables for custom configuration
-(setq
-	site-lisp-dir (expand-file-name "site-lisp" user-emacs-directory)
-	elisp-dir (expand-file-name "elisp" user-emacs-directory))
-
+(setq elisp-dir (expand-file-name "elisp" user-emacs-directory))
 ;; Set up load path
-(add-to-list 'load-path site-lisp-dir)
 (add-to-list 'load-path elisp-dir)
 
-(dolist (project (directory-files site-lisp-dir t "\\w+"))
-  (when (file-directory-p project)
-    (add-to-list 'load-path project)))
-
-(load "load-packages")
+(load "setup-packages")
 (load "general-settings")
+(load "load-packages")
 (load "init-keybindings")
 (load "setup-mu4e")
 (load "setup-magit")
@@ -33,10 +26,6 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(package-archives
-	 (quote
-		(("gnu" . "http://elpa.gnu.org/packages/")
-		 ("marmalade" . "http://marmalade-repo.org/packages/"))))
  '(puppet-indent-tabs-mode t)
  '(ruby-indent-tabs-mode t)
  '(sgml-xml-mode t)
