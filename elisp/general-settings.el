8022d2a7aa47b4e22cf235342d0ab186276a0b36
Christopher Rasch-Olsen Raa <christopher@stix.no>
2016-02-06 14:50:41 +0100

    Settings for window movement and package install path

--- elisp/general-settings.el
+++ elisp/general-settings.el
@@ -23,3 +23,12 @@
 
 ;; don't keep message buffers around
 (setq message-kill-buffer-on-exit t)
+
+;; use Shift+arrow_keys to move cursor around split panes
+(windmove-default-keybindings)
+
+;; when cursor is on edge, move to the other side, as in a torus space
+(setq windmove-wrap-around t )
+
+;; Set emacs to install packages to ~/.elpa/
+(setq package-user-dir (expand-file-name ".elpa" "~"))
