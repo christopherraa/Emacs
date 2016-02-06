13c0f747d4a5011247262f12485ecb7e49b02de2
Christopher Rasch-Olsen Raa <christopher@stix.no>
2016-02-06 15:35:40 +0100

    Setting magit font faces to be more appealing to me.

--- elisp/setup-magit.el
+++ elisp/setup-magit.el
@@ -1,9 +1,35 @@
 ; Settings for Magit
-(defun disable-magit-highlight-in-buffer () 
-  (face-remap-add-relative 'magit-item-highlight '())
-	(set-face-background 'magit-diff-hunk-header "black")
-	(set-face-background 'magit-diff-file-header "black")
-)
-(add-hook 'magit-status-mode-hook 'disable-magit-highlight-in-buffer)
+(defun my-magit-defaults ()
+	;; Styling of lines of code added
+	(set-face-attribute 'magit-diff-added nil
+											:background nil
+											:foreground "green")
+	(set-face-attribute 'magit-diff-added-highlight nil
+											:background nil
+											:foreground "green")
+	;; Styling of lines of code removed
+	(set-face-attribute 'magit-diff-removed nil
+											:background nil
+											:foreground "#aa2222")
+	(set-face-attribute 'magit-diff-removed-highlight nil
+											:background nil
+											:foreground "#aa2222")
 
+	(set-face-attribute 'magit-diff-context-highlight nil
+											:background nil
+											:foreground "white")
+	(set-face-attribute 'magit-diff-context nil
+											:foreground "white")
+	(set-face-attribute 'magit-diff-file-heading-highlight nil
+											:foreground "black")
+
+	;; Styling header for hunks
+	(set-face-attribute 'magit-diff-hunk-heading nil
+											:background "#414141"
+											:foreground "white")
+	(set-face-attribute 'magit-diff-hunk-heading-highlight nil
+											:background "#414141"
+											:foreground "#ffc56b")
+	)
+(add-hook 'magit-status-mode-hook 'my-magit-defaults)
 (global-set-key (kbd "C-c g") 'magit-status)
