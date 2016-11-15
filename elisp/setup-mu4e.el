d835d1d82f203c963a6ed204c6853ac64797334d
Christopher Rasch-Olsen Raa <christopher@stix.no>
2016-11-15 10:24:56 +0100

    Some minor email-related settings.
    Also removing yasnippet setup.

--- elisp/setup-mu4e.el
+++ elisp/setup-mu4e.el
@@ -1,3 +1,9 @@
+(require 'mu4e)
+(defun file-string (file)
+	"Read the contents of a file and return as a string."
+	(with-current-buffer (find-file-noselect file)
+		(buffer-string)))
+
 (setq
 	mu4e-maildir "~/Maildir"
 	mu4e-drafts-folder "/INBOX.Drafts"
@@ -18,11 +24,14 @@
 	mu4e-get-mail-command "/bin/true"
 	mu4e-update-interval 30
 	mu4e-use-fancy-chars t
-	mu4e-html2text-command "html2text"
+	mu4e-html2text-command "w3m -T text/html"
 	mu4e-user-mail-address-list (list "christopher@stix.no" "christopher.raa@stix.no")
 
 	user-mail-address "christopher@stix.no"
-	message-signature-file "~/.email-signature.txt"
+	mu4e-reply-to-address "christopher@stix.no"
+	user-full-name  "Christopher Rasch-Olsen Raa"
+
+	mu4e-compose-signature (file-string "~/.email-signature.txt")
 	message-send-mail-function 'smtpmail-send-it
 	smtpmail-stream-type 'ssl
 	smtpmail-default-smtp-server "secure.emailsrvr.com"
--- elisp/setup-yasnippet-mode.el
+++
@@ -1,3 +0,0 @@
-; Load custom yasnippets
-(setq yas/root-directory (expand-file-name "snippets" user-emacs-directory))
-(yas/load-directory yas/root-directory)
