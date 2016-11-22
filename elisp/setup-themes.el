7935f0893facc4f73e9ddc982d686022c6650010
Christopher Rasch-Olsen Raa <christopher@stix.no>
2016-11-22 10:52:00 +0100

    Prettier vertical separator between panes.

--- elisp/setup-themes.el
+++ elisp/setup-themes.el
@@ -1,2 +1,6 @@
+;; Making the vertical divider between panes a little prettier
+(set-display-table-slot standard-display-table 'vertical-border ?│)
+
+;; Actual themeing
 (add-to-list 'custom-theme-load-path "~/.emacs.d/themes/")
 (load-theme 'wombat t)
