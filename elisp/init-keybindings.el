bc74578b574830367d52ec27494e5a77640d1967
Christopher Rasch-Olsen Raa <christopher@stix.no>
2021-10-23 07:06:54 +0200

    Keybinding to move whole lines up or down

--- elisp/init-keybindings.el
+++ elisp/init-keybindings.el
@@ -4,3 +4,22 @@
 (global-set-key "\M-[1;5B"    'forward-paragraph) ; Ctrl+down    => jumping "down" one paragraph
 (global-set-key "\M-[1;5C"    'forward-word)  ; Ctrl+right   => forward word
 (global-set-key "\M-[1;5D"    'backward-word) ; Ctrl+left    => backward word
+
+(defun move-line-up ()
+  "Move up the current line."
+  (interactive)
+  (transpose-lines 1)
+  (forward-line -2)
+  (indent-according-to-mode))
+
+
+(defun move-line-down ()
+  "Move down the current line."
+  (interactive)
+  (forward-line 1)
+  (transpose-lines 1)
+  (forward-line -1)
+  (indent-according-to-mode))
+
+(global-set-key [(control meta up)]  'move-line-up)
+(global-set-key [(control meta down)]  'move-line-down)
