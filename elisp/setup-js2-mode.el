d47a4869fde3117f6296f197c5133806bb296ebd
Christopher Rasch-Olsen Raa <christopher@stix.no>
2021-10-23 07:08:09 +0200

    Add some settings for javascript modes, can't remember why

--- elisp/setup-js2-mode.el
+++ elisp/setup-js2-mode.el
@@ -1,6 +1,9 @@
 (require 'js2-mode)
 (add-to-list 'auto-mode-alist '("\\.js\\'" . js2-mode))
+(add-to-list 'auto-mode-alist '("\\.es6\\'" . js2-mode))
 (setq
+ js2-strict-missing-semi-warning nil
+ js2-missing-semi-one-line-override t
  js-indent-level 2
  js2-basic-offset 2
  js2-bounce-indent-p t
