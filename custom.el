7c8cdcd51db2eb5d1363dd579e801a9cfdb131c3
Christopher Rasch-Olsen Raa <christopher@stix.no>
2021-10-23 19:12:27 +0200

    Added some default structures to custom.el

--- custom.el
+++ custom.el
@@ -1,3 +1,9 @@
+;;; custom.el --- Variables set by Custom
+
+;;; Commentary:
+
+;;; Code:
+
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
@@ -10,3 +16,5 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  )
+
+;;; custom.el ends here
