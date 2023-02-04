1b17802dc45149352ebef4946bb5a80af1006b24
Christopher Rasch-Olsen Raa <christopher@stix.no>
2023-02-04 08:06:02 +0100

    Removing marmalade

--- elisp/setup-packages.el
+++ elisp/setup-packages.el
@@ -7,7 +7,6 @@
 
 (require 'package)
 (add-to-list 'package-archives '("gnu" . "http://elpa.gnu.org/packages/"))
-(add-to-list 'package-archives '("marmalade" . "http://marmalade-repo.org/packages/"))
 (add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/"))
 (package-initialize)
 
